529ef64f4dff5b5de7720eea898a60d27a71d8ee
bhuvanpatle <bhuvanpatle2003@gmail.com>
2025-02-14 21:19:44 +0530

    first plot

---
+++ plot1.R
@@ -0,0 +1,20 @@
+# Read in the power consumption dataset
+powerData <- read.table("./exdata_data_household_power_consumption/household_power_consumption.txt", 
+                        header = TRUE, 
+                        sep = ";", 
+                        stringsAsFactors = FALSE, 
+                        dec = ".")
+
+# Subset data for the dates 1/2/2007 and 2/2/2007
+filteredData <- powerData[powerData$Date %in% c("1/2/2007", "2/2/2007"), ]
+
+# Convert the Global_active_power column to numeric
+globalActPower <- as.numeric(filteredData$Global_active_power)
+
+# Open PNG device, create the histogram, and then close the device
+png("plot1.png", width = 480, height = 480)
+hist(globalActPower, 
+     col = "red", 
+     main = "Global Active Power", 
+     xlab = "Global Active Power (kilowatts)")
+dev.off()
